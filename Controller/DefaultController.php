3b2897f76740749676de66a84de60a15613f3045
Didier Ungar <didungar@gmail.com>
2017-01-22 18:20:36 +0100

    Premier jet d'utilisation de l'api, il faudra extrapoler la base de la route

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -22,6 +22,20 @@ class DefaultController extends Controller
 	 */
 	public function writeAction()
 	{
-		return [];
+		$oApi = new \DidUngar\ApiClientBundle\Services\ApiClientService($this->container);
+		if ( !empty($_POST) ) {
+			$oApi->query('/blog/post/new.json', [
+				'id_categ' => $_POST['id_categ'],
+				'title' => $_POST['title'],
+				'slug' => $_POST['slug'],
+				'descri' => $_POST['descri'],
+				'text' => $_POST['text'],
+				'id_user' => $_POST['id_user'],
+			]);
+		}
+		return [
+			'lstCateg' => $oApi->query('/blog/categ.json')->response,
+			'lstAuthor' => $oApi->query('/blog/author.json')->response,
+		];
 	}
 }
